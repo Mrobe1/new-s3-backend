6acc175600c359ea38ef12cace1b72f495cb65bd
mrobe1 <michael.roberts@cloudreach.com>
2022-03-24 20:38:42 -0400

    setting up backend to work with terraform

--- .main.tf
+++ .main.tf
@@ -1,16 +1,20 @@
 resource "aws_s3_bucket" "my_state_bucket" {
-  bucket = "talent-academy-account_id-tfstates-michael"
-
-  versioning {
-    enabled = true
-  }
+  bucket = "John1-816606439534-tfstates"
 
   lifecycle {
     prevent_destroy = true
   }
 
   tags = {
-    Name        = "talent-academy-tfstates"
-    Environment = "Test"
+      Name = "John1-816606439534-tfstates"
+      Environment = "Test"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "version_my_bucket" {
+  bucket = aws_s3_bucket.my_state_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
   }
 }
